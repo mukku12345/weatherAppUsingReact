dcfd7540d6c442f2c337cb99d57800095ef8eee5
Mukesh KUSHWAHA <101133241+mukku12345@users.noreply.github.com>
2022-10-20 17:23:43 +0530

    Update Tempapp.jsx

--- src/components/Tempapp.jsx
+++ src/components/Tempapp.jsx
@@ -7,9 +7,11 @@ export const Tempapp = () => {
     const[city,setCity]=useState(null);
     const[search,setSearch]=useState("Delhi");
 
+   
     const fetchWeather = async (e)=>{
         e.preventDefault();
-        const url=`http://api.weatherstack.com/current?access_key=1c8541cdb10f2d5719a07207dbfb256e&query=${search}`
+        // const url=`http://api.weatherstack.com/current?access_key=1c8541cdb10f2d5719a07207dbfb256e&query=${search}`
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=5fe394974129a6185ed442c85dfc7878`
          const response=await fetch(url)
          const data=await response.json()
          setCity(data);
@@ -45,26 +47,27 @@ export const Tempapp = () => {
     <div className='tempandimage'>
 
      <div className='temprature'>
-    <h1>{city.current.temperature}<span>&deg;C</span></h1>
-    <p className='feelsLike'>Feels Like  {city.current.feelslike}&deg;C</p>
+    <h1>{city.main.temp}<span>&deg;C</span></h1>
+    <p className='feelsLike'>Feels Like  {city.main.feels_like}&deg;C</p>
      </div>
 
     <div className='location'>
    <p>
      <i className="fa-solid fa-street-view"></i>
-    {search},{city.location.country}
+    {search},{city.sys.country}
     </p>
     </div>
 
 <div className='icon'>
-<img src={city.current.weather_icons} alt='weather'/>
+    
+{/* <img src={city.current.weather_icons} alt='weather'/> */}
 </div>
 
 </div>
 
 <div className='date'>
-<h3>{city.location.localtime}</h3>
-<p>{city.current.weather_descriptions}</p>
+{/* <h3>{city.location.localtime}</h3> */}
+
 </div>
 
 </div>
@@ -72,21 +75,21 @@ export const Tempapp = () => {
 
 <div className='current-section'>
 <div className='current'>
+<p className='mintemp'>Min Temp</p>
+<p className='maxtemp'>Max Temp</p>
 <p className='humidity'>Humidity</p>
 <p className='wind-speed'>Wind-speed</p>
-<p className='wind direction'>Wind-direction</p>
 <p className='pressure'>Pressure</p>
-<p className='uv'>UV index</p>
 <p className='visibility'>Visibility</p>
 </div>
 
 <div className='current'>
-<p className='humidity'>{city.current.humidity}%</p>
-<p className='Wind-speed'>{city.current.wind_speed} km/h</p>
-<p className='Wind-direction'>From  {city.current.wind_dir}</p>
-<p className='Pressure'>{city.current.pressure} mBar</p>
-<p className='uv'>{city.current.uv_index}</p>
-<p className='visibility'>{city.current.visibility} km</p>
+    <p className='mintemp'>{city.main.temp_min}</p>
+    <p className='maxtemp'>{city.main.temp_max}</p>
+<p className='humidity'>{city.main.humidity}%</p>
+<p className='Wind-speed'>{city.wind.speed} km/h</p>
+<p className='Pressure'>{city.main.pressure} mBar</p>
+<p className='visibility'>{city.visibility} m</p>
 </div>
 </div>
 
